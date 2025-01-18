0cf9af8e7a3e5003329a9185dfeb04f10ec28dec
Tanush Arora <tanush935@gmail.com>
2025-01-19 01:21:05 +0530

    fourty three commit

---
+++ Binarysearch/Binarysearchlecture2/03_sqrt.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+//using the approach search space create 
+//we can focus on search space 
+//possible answer for 64 is the search space 
+int mysqrt(int n){
+    int s = 0;
+    int e = n - 1;
+    int mid = ( s + e )/2;
+    int ans = -1;
+    while(s <= e){
+        if(mid*mid  <=  n){
+            ans = mid;
+            s = mid + 1;
+        }
+        else{
+            e = mid - 1;
+        }
+        mid = (s + e)/2;
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter the number for which u find the square root : ";
+    cin>>n;
+    int ans = mysqrt(n);
+    double ans1 = sqrt(n);
+    cout<<"The sqrt for this following number is : "<<ans<<endl;
+    cout<<"The sqrt for this following number is : "<<fixed<<setprecision(10)<<ans<<endl;
+    return 0;
+}
